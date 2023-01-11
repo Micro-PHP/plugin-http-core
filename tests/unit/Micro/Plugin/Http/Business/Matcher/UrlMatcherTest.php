1b3a35175ac66d50c2c9b6f75b9b4ea3180d4651
Stanislau Komar <stanislau_komar@epam.com>
2023-01-11 22:32:09 +0400

    Phpunit: add locator test

---
+++ tests/unit/Micro/Plugin/Http/Business/Matcher/UrlMatcherTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Micro framework package.
+ *
+ * (c) Stanislau Komar <kost@micro-php.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Micro\Plugin\Http\Business\Matcher;
+
+use PHPUnit\Framework\TestCase;
+
+class UrlMatcherTest extends TestCase
+{
+
+    public function testMatch()
+    {
+
+    }
+}
